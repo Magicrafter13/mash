b3415094e057e610a7028172221493964879abde
Matthew Rease <scubaventure101@hotmail.com>
2022-02-09 17:11:35 -0800

    Organize (Don't compile)
    this code won't compile, just doing this in a git friendly way

--- include/hashTable.h
+++ include/hashTable.h
@@ -1,4 +1,39 @@
-#include "node.h"
+#ifndef MR_HASHTABLE_H
+#define MR_HASHTABLE_H
+
+/**************
+ * Structures *
+ **************/
+
+struct strhash {
+	unsigned long long hash;
+	char *str;
+	void *data;
+};
+
+/*
+ * Linked list node.
+ * Stores a pointer to a hashed string, and the next node in the list.
+ */
+struct node {
+	struct strhash *data;
+	struct node *temp;
+	struct node *next;
+};
+
+// Initialize a new sentinel node.
+struct node *init();
+
+// Add a new value into the list of nodes.
+int add(struct node*, struct strhash*);
+
+// Checks for a value in the list.
+struct node *search(struct node*, unsigned long long, char*);
+
+int removeNode(struct node*, unsigned long long, char*);
+
+// Free all nodes in a list from memory.
+void free_nodes(struct node*);
 
 #define CHECK_AVERAGE 0 // Whether or not to check the average collisions
 
@@ -20,3 +55,5 @@ hashTable *tableAdd(hashTable*, unsigned long long*, char*, struct strhash**);
 struct strhash *tableSearch(hashTable*, unsigned long long, char*);
 
 hashTable *tableRemove(hashTable*, unsigned long long*, char*);
+
+#endif
